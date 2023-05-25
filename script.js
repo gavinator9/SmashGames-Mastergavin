bbfc8caa54e617dd4506616401b4a5ea9a078828
Jesse Harlan <gotsanity@gmail.com>
2023-05-25 11:57:47 -0700

    added data to script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-alert("hello world");
+const data = {
+    cards: [
+        { 
+            imgPath: "./somewhere.img",
+            imgAltText: "some alternate text",
+            title: "The Title",
+            body: "Some kind of body of text",
+            actionLink: "https://localhost:5500/games/game.html"
+        },
+        { 
+            imgPath: "./somewhere.img",
+            imgAltText: "some alternate text",
+            title: "The Title",
+            body: "Some kind of body of text",
+            actionLink: "https://localhost:5500/games/game.html"
+        },
+        { 
+            imgPath: "./somewhere.img",
+            imgAltText: "some alternate text",
+            title: "The Title",
+            body: "Some kind of body of text",
+            actionLink: "https://localhost:5500/games/game.html"
+        }
+    ]
+}
