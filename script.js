9088a6d5d5d8fe500e60b7a8854e98737b367412
Gavin Elliott <epikdude@rocketmail.com>
2023-05-31 10:57:05 -0700

    fixd it

---
+++ script.js
@@ -0,0 +1,26 @@
+// index.html Smash Games - Home
+// blast.html Smash Games - Inferno Blast
+const data={
+    brandname: "Smash Games",
+    pages: [
+        {
+            name: "index",
+            pagename: "Home",
+            calltoaction: {
+                src: "images/SmashGamesLogo-short.png",
+                alttext: "smash games logo"
+            }
+        },
+        {
+            name:"blast",
+            pagename:"Inferno Blast",
+            calltoaction: {
+                src: "images/Inferno-Jumbotron.png",
+                alttext: "Inferno Blast Gameplay",
+                buttonlinksrc: "https://steampowered.com",
+                buttonlinktext: "buy now on steam!"
+            }
+        }
+
+    ]
+}
